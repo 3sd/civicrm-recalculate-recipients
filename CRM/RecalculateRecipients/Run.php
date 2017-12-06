fb4c55b24a42876e8c3f6579f725a158de636dde
Michael McAndrew <michaelmcandrew@thirdsectordesign.org>
2017-12-06 20:29:33 +0000

    initial commit

---
+++ CRM/RecalculateRecipients/Run.php
@@ -0,0 +1,16 @@
+<?php
+class CRM_RecalculateRecipients_Run{
+  static function run(){
+
+    $currentTime = date('YmdHis');
+    $jobs = civicrm_api3('MailingJob', 'get', ['status' =>  'scheduled', 'scheduled_date' => ['<=' => $currentTime]]);
+    foreach($jobs['values'] as $job){
+      $mailings[] = $job['mailing_id'];
+    }
+    $mailings = array_unique($mailings);
+    foreach($mailings as $mailing_id){
+      $mailing = civicrm_api3('mailing', 'getsingle', ['id' => $mailing_id]);
+      CRM_Mailing_BAO_Mailing::getRecipients($mailing['id'], $mailing['id'], TRUE, $mailing['dedupe_email'], NULL);
+    }
+  }
+}
