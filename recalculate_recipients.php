3764e17ab30e1c9a9f091242779f732d11461ab1
Jamie McClelland <jm@mayfirst.org>
2024-07-10 10:02:00 -0400

    civix upgrade

--- recalculate_recipients.php
+++ recalculate_recipients.php
@@ -22,24 +22,6 @@ function recalculate_recipients_civicrm_install() {
 }
 
 /**
- * Implements hook_civicrm_postInstall().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_postInstall
- */
-function recalculate_recipients_civicrm_postInstall() {
-  _recalculate_recipients_civix_civicrm_postInstall();
-}
-
-/**
- * Implements hook_civicrm_uninstall().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_uninstall
- */
-function recalculate_recipients_civicrm_uninstall() {
-  _recalculate_recipients_civix_civicrm_uninstall();
-}
-
-/**
  * Implements hook_civicrm_enable().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_enable
@@ -48,37 +30,10 @@ function recalculate_recipients_civicrm_enable() {
   _recalculate_recipients_civix_civicrm_enable();
 }
 
-/**
- * Implements hook_civicrm_disable().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_disable
- */
-function recalculate_recipients_civicrm_disable() {
-  _recalculate_recipients_civix_civicrm_disable();
-}
-
-/**
- * Implements hook_civicrm_upgrade().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_upgrade
- */
-function recalculate_recipients_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _recalculate_recipients_civix_civicrm_upgrade($op, $queue);
-}
-
 function recalculate_recipients_civicrm_apiWrappers(&$wrappers, $apiRequest) {
   //&apiWrappers is an array of wrappers, you can add your(s) with the hook.
   // You can use the apiRequest to decide if you want to add the wrapper (eg. only wrap api.Contact.create)
   if ($apiRequest['entity'] == 'Job' && $apiRequest['action'] == 'process_mailing') {
     $wrappers[] = new CRM_RecalculateRecipients_Wrapper();
   }
-}
-
-/**
- * Implements hook_civicrm_entityTypes().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_entityTypes
- */
-function recalculate_recipients_civicrm_entityTypes(&$entityTypes) {
-  _recalculate_recipients_civix_civicrm_entityTypes($entityTypes);
 }
